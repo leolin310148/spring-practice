f936a1c9d24260b2576457b23bf88a639928f0e5
Leo Lin <leolin310148@gmail.com>
2014-01-04 23:38:38 +0800

    First practice
    A simple practice using SpringFramework

---
+++ 1_Hello-SpringFramework/src/main/java/org/noteverything/practice/HelloSpringApp.java
@@ -0,0 +1,37 @@
+package org.noteverything.practice;
+
+/**
+ * Created by leolin on 2014/1/4.
+ */
+public class HelloSpringApp {
+
+    //傳統的Java Bean使用方式
+    //General way using Java Bean
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.setBookName("This is a Java book");
+        System.out.println(book.getBookName());
+    }
+
+
+    //透過xml檔設定讓spring幫你產生book這個物件，並同時協助你設定book的bookName
+    //set the book Bean from spring-config.xml
+    //get the book Bean from applicationContext and use it.
+//    public static void main(String[] args) {
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+//        Book book = applicationContext.getBean(Book.class);
+//        System.out.println(book.getBookName());
+//    }
+
+    //另一種取得Bean的方式，是根據<bean id="book" ...  的id來取得指定的Bean
+    //get the book Bean By the "id" defined in the spring-config.xml
+//    public static void main(String[] args) {
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+//        Book book = (Book) applicationContext.getBean("book");
+//        System.out.println(book.getBookName());
+//
+//        Book anotherBook = (Book) applicationContext.getBean("book2");
+//        System.out.println(anotherBook.getBookName());
+//    }
+
+}
